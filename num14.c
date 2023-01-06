332ecf632e80f768d28d6977fc09650f2cf3479d
piarochang <121932265+piarochang@users.noreply.github.com>
2023-01-06 10:20:26 +0800

    第14題

---
+++ num14.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int times;
+    scanf("%d",&times);
+    for(int i = 0;i <  times;i++){
+       printf("%d.I don't say swear words!\n",i+1);
+    }
+    return 0;
+}
